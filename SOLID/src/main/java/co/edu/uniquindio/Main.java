58fec7ccacccdca23d5722f3ec9480ed046ce0a9
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2024-08-27 16:25:09 -0500

    Corrección Joption

--- SOLID/src/main/java/co/edu/uniquindio/Main.java
+++ SOLID/src/main/java/co/edu/uniquindio/Main.java
@@ -1,40 +1,49 @@
 package co.edu.uniquindio;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public class MenuInteractivo {
-
-        Empresa empresa  = new Empresa();
-        private static List<Empleado> empleados = new ArrayList<>();
-        private static List<Departamento> departamentos = new ArrayList<>();
-        private static List<Proyecto> proyectos = new ArrayList<>();}
-
         public static void main(String[] args) {
-        Departamento depto1 = new Departamento("Tecnología", "D002");
-        Gerente gerente = new Gerente("Alice", "1",depto1 );
-        Tecnico tecnico = new Tecnico("Bob", "2", "Procesamiento de datos", depto1);
+            Departamento depto1 = new Departamento("Tecnología", "D002");
+            Gerente gerente = new Gerente("Alice", "1", depto1);
+            Tecnico tecnico = new Tecnico("Bob", "2", "Procesamiento de datos", depto1);
 
-        Departamento depto = new Departamento("Tecnología", "D001");
-        depto.agregarEmpleado(tecnico);
+            Departamento depto = new Departamento("Tecnología", "D001");
+            depto.agregarEmpleado(tecnico);
 
-        Proyecto proyecto = new Proyecto("Desarrollo de Software", "P001");
-        proyecto.asignarGerente(gerente);
-        proyecto.asignarEmpleado(tecnico);
+            Proyecto proyecto = new Proyecto("Desarrollo de Software", "P001");
+            proyecto.asignarGerente(gerente);
+            proyecto.asignarEmpleado(tecnico);
 
-        System.out.println(depto);
-        System.out.println(proyecto);
+            System.out.println(depto);
+            System.out.println(proyecto);
+            Empresa empresa = new Empresa();
+            empresa.agregarDepartamentos(depto1);
+            empresa.agregarDepartamentos(depto);
+            empresa.agregarProyectos(proyecto);
+            empresa.agregarEmpleados(gerente);
+            empresa.agregarEmpleados(tecnico);
 
-        gerente.contribuir();
-        tecnico.contribuir();
+            gerente.contribuir();
+            tecnico.contribuir();
 
-            while (true) {
-                String[] opciones = {
+            MenuInteractivo menu = new MenuInteractivo(empresa);
+            menu.iniciarMenu();
+        }
+
+    public static class MenuInteractivo {
+            private static Empresa empresa;
+
+            public MenuInteractivo(Empresa empresa) {
+                this.empresa = empresa;
+            }
+
+            public void iniciarMenu() {
+                while (true) {
+                    String[] opciones = {
                         "Añadir Empleado",
                         "Añadir Técnico",
                         "Añadir Gerente",
@@ -46,13 +55,13 @@ public class Main {
                         "Mostrar Datos",
                         "Salir"
                 };
-                
+
                 int eleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción", "Menú Interactivo",
                         JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
 
                 switch (eleccion) {
                     case 0:
-                        agregarEmpleadoNormal();
+                        agregarEmpleado();
                         break;
                     case 1:
                         agregarTecnico();
@@ -84,73 +93,74 @@ public class Main {
                         break;
                     default:
                         JOptionPane.showMessageDialog(null, "Opción no válida.");
+                        System.exit(0);
                 }
             }
         }
 
-        private static void agregarEmpleadoNormal() {
+        private static void agregarEmpleado() {
             String nombre = JOptionPane.showInputDialog("Ingrese el nombre del empleado:");
-            int idEmpleado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID del empleado:"));
+            String idEmpleado = JOptionPane.showInputDialog("Ingrese el ID del empleado:");
             String codigoDepartamento = JOptionPane.showInputDialog("Ingrese el código del departamento del empleado:");
 
-            Departamento departamento = MenuInteractivo.departamentos.stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
+            Departamento departamento = empresa.getListDepartamentos().stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
             if (departamento == null) {
                 JOptionPane.showMessageDialog(null, "Departamento no encontrado.");
                 return;
             }
 
             Empleado empleado = new Empleado(nombre, idEmpleado, departamento);
-            MenuInteractivo.empleados.add(empleado);
+            empresa.agregarEmpleados(empleado);
         }
 
         private static void agregarTecnico() {
             String nombre = JOptionPane.showInputDialog("Ingrese el nombre del técnico:");
-            int idEmpleado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID del técnico:"));
+            String idEmpleado = JOptionPane.showInputDialog("Ingrese el ID del técnico:");
             String codigoDepartamento = JOptionPane.showInputDialog("Ingrese el código del departamento del técnico:");
             String especialidad = JOptionPane.showInputDialog("Ingrese la especialidad del técnico:");
 
-            Departamento departamento = MenuInteractivo.departamentos.stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
+            Departamento departamento = empresa.getListDepartamentos().stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
             if (departamento == null) {
                 JOptionPane.showMessageDialog(null, "Departamento no encontrado.");
                 return;
             }
 
             Tecnico tecnico = new Tecnico(nombre, idEmpleado, especialidad, departamento);
-            MenuInteractivo.empleados.add(tecnico);
+            empresa.agregarEmpleados(tecnico);
         }
 
         private static void agregarGerente() {
             String nombre = JOptionPane.showInputDialog("Ingrese el nombre del gerente:");
-            int idEmpleado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID del gerente:"));
+            String idEmpleado = JOptionPane.showInputDialog("Ingrese el ID del gerente:");
             String codigoDepartamento = JOptionPane.showInputDialog("Ingrese el código del departamento del gerente:");
 
-            Departamento departamento = MenuInteractivo.departamentos.stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
+            Departamento departamento = empresa.getListDepartamentos().stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
             if (departamento == null) {
                 JOptionPane.showMessageDialog(null, "Departamento no encontrado.");
                 return;
             }
 
             Gerente gerente = new Gerente(nombre, idEmpleado, departamento);
-            MenuInteractivo.empleados.add(gerente);
+            empresa.agregarEmpleados(gerente);
         }
 
         private static void agregarDepartamento() {
             String nombre = JOptionPane.showInputDialog("Ingrese el nombre del departamento:");
             String codigo = JOptionPane.showInputDialog("Ingrese el código del departamento:");
             Departamento departamento = new Departamento(nombre, codigo);
-            MenuInteractivo.departamentos.add(departamento);
+            empresa.agregarDepartamentos(departamento);
         }
 
         private static void agregarProyecto() {
             String nombre = JOptionPane.showInputDialog("Ingrese el nombre del proyecto:");
             String codigo = JOptionPane.showInputDialog("Ingrese el código del proyecto:");
             Proyecto proyecto = new Proyecto(nombre, codigo);
-            MenuInteractivo.proyectos.add(proyecto);
+            empresa.agregarProyectos(proyecto);
         }
 
         private static void asignarEmpleadoADepartamento() {
             String codigoDepartamento = JOptionPane.showInputDialog("Ingrese el código del departamento al que desea asignar el empleado:");
-            Departamento departamento = MenuInteractivo.departamentos.stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
+            Departamento departamento = empresa.getListDepartamentos().stream().filter(d -> d.getCodigo().equals(codigoDepartamento)).findFirst().orElse(null);
 
             if (departamento == null) {
                 JOptionPane.showMessageDialog(null, "Departamento no encontrado.");
@@ -158,7 +168,7 @@ public class Main {
             }
 
             String idEmpleado = JOptionPane.showInputDialog("Ingrese el ID del empleado a asignar:");
-            Empleado empleado = MenuInteractivo.empleados.stream().filter(e -> e.getIdEmpleado() == Integer.parseInt(idEmpleado)).findFirst().orElse(null);
+            Empleado empleado = empresa.getListEmpleados().stream().filter(e -> e.getIdEmpleado().equals(idEmpleado)).findFirst().orElse(null);
 
             if (empleado == null) {
                 JOptionPane.showMessageDialog(null, "Empleado no encontrado.");
@@ -170,7 +180,7 @@ public class Main {
 
         private static void asignarEmpleadoAProyecto() {
             String codigoProyecto = JOptionPane.showInputDialog("Ingrese el código del proyecto al que desea asignar el empleado:");
-            Proyecto proyecto = MenuInteractivo.proyectos.stream().filter(p -> p.getCodigo().equals(codigoProyecto)).findFirst().orElse(null);
+            Proyecto proyecto = empresa.getListProyectos().stream().filter(p -> p.getCodigo().equals(codigoProyecto)).findFirst().orElse(null);
 
             if (proyecto == null) {
                 JOptionPane.showMessageDialog(null, "Proyecto no encontrado.");
@@ -178,7 +188,7 @@ public class Main {
             }
 
             String idEmpleado = JOptionPane.showInputDialog("Ingrese el ID del empleado a asignar:");
-            Empleado empleado = MenuInteractivo.empleados.stream().filter(e -> e.getIdEmpleado() == Integer.parseInt(idEmpleado)).findFirst().orElse(null);
+            Empleado empleado = empresa.getListEmpleados().stream().filter(e -> e.getIdEmpleado().equals( idEmpleado)).findFirst().orElse(null);
 
             if (empleado == null) {
                 JOptionPane.showMessageDialog(null, "Empleado no encontrado.");
@@ -190,7 +200,7 @@ public class Main {
 
         private static void asignarGerenteAProyecto() {
             String codigoProyecto = JOptionPane.showInputDialog("Ingrese el código del proyecto al que desea asignar el gerente:");
-            Proyecto proyecto = MenuInteractivo.proyectos.stream().filter(p -> p.getCodigo().equals(codigoProyecto)).findFirst().orElse(null);
+            Proyecto proyecto = empresa.getListProyectos().stream().filter(p -> p.getCodigo().equals(codigoProyecto)).findFirst().orElse(null);
 
             if (proyecto == null) {
                 JOptionPane.showMessageDialog(null, "Proyecto no encontrado.");
@@ -199,7 +209,7 @@ public class Main {
 
             String idGerente = JOptionPane.showInputDialog("Ingrese el ID del gerente a asignar (deje en blanco si no hay):");
             if (!idGerente.trim().isEmpty()) {
-                Gerente gerente = (Gerente) MenuInteractivo.empleados.stream().filter(e -> e instanceof Gerente && e.getIdEmpleado() == Integer.parseInt(idGerente)).findFirst().orElse(null);
+                Gerente gerente = (Gerente) empresa.getListEmpleados().stream().filter(e -> e instanceof Gerente && e.getIdEmpleado().equals(idGerente)).findFirst().orElse(null);
                 if (gerente == null) {
                     JOptionPane.showMessageDialog(null, "Gerente no encontrado.");
                     return;
@@ -209,12 +219,13 @@ public class Main {
         }
 
         private static void mostrarDatos() {
-            String departamentosStr = MenuInteractivo.departamentos.stream().map(Departamento::toString).collect(Collectors.joining("\n\n"));
-            String proyectosStr = MenuInteractivo.proyectos.stream().map(Proyecto::toString).collect(Collectors.joining("\n\n"));
-            String empleadosStr = MenuInteractivo.empleados.stream().map(Empleado::toString).collect(Collectors.joining("\n\n"));
+            String departamentosStr = empresa.getListDepartamentos().stream().map(Departamento::toString).collect(Collectors.joining("\n\n"));
+            String proyectosStr = empresa.getListProyectos().stream().map(Proyecto::toString).collect(Collectors.joining("\n\n"));
+            String empleadosStr = empresa.getListEmpleados().stream().map(Empleado::toString).collect(Collectors.joining("\n\n"));
 
             String mensaje = "Departamentos:\n" + departamentosStr + "\n\nProyectos:\n" + proyectosStr + "\n\nEmpleados:\n" + empleadosStr;
             JOptionPane.showMessageDialog(null, mensaje);
         }
     }
+}
 
